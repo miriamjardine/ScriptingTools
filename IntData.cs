e9debcff38c952a470988b58b69f81f8d216c978
Miriam <miajardine1@gmail.com>
2022-09-01 16:40:31 -0600

    Int Data, Obstacles

---
+++ IntData.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+
+public class IntData : ScriptableObject
+{
+    public int value;
+
+    public void UpdateValue(int num)
+    {
+        value += num;
+    }
+
+    public void ReplaceValue(int num)
+    {
+        value = num;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        img.fillAmount = value;
+    }
+
+    public void DisplayNumber(Text txt)
+    {
+        txt.text = value.ToString();
+    }
+}

dc796d2a6b40566c9d615651bb6c285dff3aa9e3
Miriam <miajardine1@gmail.com>
2023-01-25 13:44:00 -0700

    LoadSceneBahaviour
    Easily changed for each game load scene behaviour script

---
+++ Behaviours/LoadSceneBehaviour.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneBehaviour : MonoBehaviour
+{
+    public void LoadCarOne()
+    {
+        SceneManager.LoadScene("Car1", LoadSceneMode.Single);
+    }
+
+}

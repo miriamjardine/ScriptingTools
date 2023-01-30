27853aa95463421f699cca12193ad7bb8d2e2699
Miriam <miajardine1@gmail.com>
2023-01-30 09:45:20 -0700

    Trying to work on movement

---
+++ Behaviours/WASDControlBehaviour.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WASDControlBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    
+    public void OnDKeyDown()
+    {
+       
+    }
+}

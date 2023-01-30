2b398db514d9682d23f621d8327f2286c8a6bc88
Miriam <miajardine1@gmail.com>
2023-01-30 12:12:37 -0700

    x-y movement

---
+++ Behaviours/CharacterMovementBehaviour.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovementBehaviour : MonoBehaviour
+{
+   private CharacterController charctrl;
+   private Vector3 movement;
+   public FloatData speed;
+
+   public enum AxisInputs
+   {
+      Horizontal
+   }
+
+   public AxisInputs horizontalInput = AxisInputs.Horizontal; 
+                    
+
+   private void Awake()
+   {
+       charctrl = GetComponent<CharacterController>();
+   }
+
+   private void Update()
+   {
+       movement.Set(Input.GetAxis(horizontalInput.ToString()), 
+                    0, 
+                    0);
+       movement *= speed.value;
+       movement *= Time.deltaTime;
+       charctrl.Move(movement);
+   }
+
+}

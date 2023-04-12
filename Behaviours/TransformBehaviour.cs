4630a0bf6bd75a18411d575e9ed217110fed706f
Miriam <miajardine1@gmail.com>
2023-04-12 13:56:50 -0600

    Adding Player Mesh

--- Behaviours/TransformBehaviour.cs
+++ Behaviours/TransformBehaviour.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TransformBehaviour : MonoBehaviour
 {
     public Rigidbody otherRb;
     public void Transform(Collider other)
    {
-        var transformObj = other.transform;
-        transformObj.parent = transform;
+       var transformObj = other.transform;
+        transformObj.parent = transform; 
+        // transform.position = Vector3.zero;
         otherRb.useGravity = false;
    }
 

59a1b472e580b64c4ebf15690ce4c80a0e7b7547
Miriam <miajardine1@gmail.com>
2023-04-17 16:38:43 -0600

    CharacterFlip

---
+++ Behaviours/CharacterFlip.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFlip : MonoBehaviour
+{
+    public GameObject flipObj;
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Quaternion currentRotation = flipObj.transform.rotation;
+            Quaternion newRotation = Quaternion.Euler(0f, 270f, 0f);
+            flipObj.transform.rotation = newRotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Quaternion currentRotation = flipObj.transform.rotation;
+            Quaternion newRotation = Quaternion.Euler(0f, 90f, 0f);
+            flipObj.transform.rotation = newRotation;
+        }
+    }
+}
